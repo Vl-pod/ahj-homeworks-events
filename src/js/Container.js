c3a226cccc41b32df0fa858a532c1fb1f01b065b
Pavel Barashkov <Pav.Barashkov@yandex.ru>
2023-07-02 18:16:35 +0400

    init

---
+++ src/js/Container.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+export default class ContainerGame {
+  constructor(element) {
+    this.element = element;
+    this._items = this.element.querySelectorAll('.container-item');
+    this.countUser = document.querySelector('.count-user');
+    this.countBot = document.querySelector('.count-bot');
+
+    this.onclickItem = this.onclickItem.bind(this);
+
+    this._items.forEach((item) => {
+      item.addEventListener('click', this.onclickItem);
+    });
+  }
+
+  create() {
+    let randomIndex = this.random();
+    const containerItem = Array.from(this._items);
+    containerItem.forEach((item, index) => {
+      if (item.classList.contains('active') && randomIndex === index) {
+        randomIndex = this.random();
+      }
+      if (item.classList.contains('active')) {
+        item.classList.remove('active');
+      }
+    });
+    containerItem[randomIndex].classList.add('active');
+    this.count(this.countBot);
+    this.finish('');
+  }
+
+  random() {
+    return Math.floor(Math.random() * 15);
+  }
+
+  count(e) {
+    e.textContent = ++e.textContent;
+  }
+
+  finish(num) {
+    if (this.countUser.textContent === num) {
+      alert('Ты победил!');
+      this.countUser.textContent = 0;
+      this.countBot.textContent = 0;
+    }
+    if (this.countBot.textContent === '6') {
+      alert('Ты проиграл!');
+      this.countUser.textContent = 0;
+      this.countBot.textContent = 0;
+    }
+  }
+
+  onclickItem(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('active')) {
+      this.count(this.countUser);
+      e.target.classList.remove('active');
+    }
+  }
+}
---
+++ webpack.prod.js
@@ -0,0 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
